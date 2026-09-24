dd8360a1a8ec7187375b2eb122b1fca5f0491a9f
Clay Conn <cconn001@regis.edu>
2026-09-23 20:31:00 -0400

    Change name to Clayton Conn

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const port = 8082;
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World. My name is Vincent Danh');
+
+	  res.end('Hello World. My name is Clayton Conn, I am in the second year of my Masters!')
 });
 
 server.listen(port, hostname, () => {
